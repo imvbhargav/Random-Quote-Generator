9ecb8a2e2ea59e05f498fa78bc99f40fc86ab041
Bhargav V <87763304+imvbhargav@users.noreply.github.com>
2024-09-25 16:28:29 +0530

    Update App.js
    Update fix in content not found line 73.

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends React.Component {
 	};
 
 	render() {
-		const { quote, author, category, isLoading } = this.state;
+		const { content, author, isLoading } = this.state;
 		var colors = [
 			'#FFEEEE',
 			'#FFF6EA',
